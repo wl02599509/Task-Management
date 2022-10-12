be4838d3f0d5f5b5f92f71c1d7f042387b8722b0
Mike <wl02599509@gmail.com>
2022-10-12 23:10:54 +0800

    i18n 中英文共用

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module TaskManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = "zh-TW"
   end
 end
