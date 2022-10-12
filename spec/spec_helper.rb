4a4b4b7257600bcc6e0cb65a78c972240a6ab2ff
Mike <wl02599509@gmail.com>
2022-10-13 00:21:48 +0800

    remove require capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'Capybara'
-Capybara.default_driver = :selenium_chrome
+# require 'Capybara'
+# Capybara.default_driver = :selenium_chrome
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
