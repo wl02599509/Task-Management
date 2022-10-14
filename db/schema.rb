8b6838b293ac898d405f525cb2c8085792e28300
Mike <wl02599509@gmail.com>
2022-10-14 11:25:29 +0800

    add migration to setup DB constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_143820) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_030532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", limit: 30, null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
