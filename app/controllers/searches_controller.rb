f02ab1aa1d2dd78efe90226db0b20f905672505d
Mike <wl02599509@gmail.com>
2022-10-17 13:38:22 +0800

    fix searches controller page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    @tasks = Task.search(params["task"]["keyword"])
+    @tasks = Task.search(params["task"]["keyword"]).page(params[:page]).per(10)
   end
 end
