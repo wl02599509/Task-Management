44e5c40ec17187d3dd2ebd2f592e28da03bc365b
Mike <wl02599509@gmail.com>
2022-10-18 10:27:23 +0800

    fix task controller and rspec

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@
 class Task < ApplicationRecord
   include AASM
 
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 150 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :tasks
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
 end
