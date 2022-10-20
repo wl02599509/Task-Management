96582b2dfaf06159b7e2913b46e08b7f33a2ec15
Mike <wl02599509@gmail.com>
2022-10-20 19:39:26 +0800

    add password confirmation in sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_030231) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_103650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_030231) do
     t.datetime "updated_at", null: false
     t.string "password", default: "", null: false
     t.integer "tasks_count", default: 0
+    t.string "password_confirmation", default: "", null: false
   end
 
 end
