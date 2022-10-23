93e0483cd1e80f3499aa185ba5a580b4e1c5dd25
Mike <wl02599509@gmail.com>
2022-10-23 09:59:58 +0800

    WIP-tag

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@
 class Task < ApplicationRecord
   include AASM
   belongs_to :user, counter_cache: true
+  has_many :tags
   has_rich_text :content
 
   validates :title, presence: true, length: { maximum: 50 }
